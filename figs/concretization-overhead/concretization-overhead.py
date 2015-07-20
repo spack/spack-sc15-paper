efa45ff94678482445c938839c5ae249afa67122
Todd Gamblin <tgamblin@llnl.gov>
2015-07-19 23:05:23 -0700

    Add concretization time figure.

---
+++ figs/concretization-overhead/concretization-overhead.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env spack-python
+import time
+import numpy as np
+import spack
+from external import yaml
+from spack.spec import Spec
+
+# prime the timer
+s = Spec('qt')
+s.concretize()
+
+reps = 10
+timings = []
+
+for name in spack.db.all_package_names():
+    print name
+    specs = [Spec(s) for s in [name] * reps]
+    times = []
+
+    for s in specs:
+        start = time.clock()
+        s.concretize()
+        end = time.clock()
+        times.append(end - start)
+
+    record = (name, s.size, times)
+    print record
+    timings.append(record)
+
+
+with open('timings.yaml', 'w') as stream:
+    yaml.dump(stream)
---
+++ figs/concretization-overhead/timings.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env spack-python
+
+from external import yaml
+import matplotlib.pyplot as plt
+
+files = { 'macbook.yaml' : 'MacBook Pro, Haswell, 2.6Ghz',
+          'merl.yaml'    : 'Linux, SandyBridge, 2.6Ghz'}
+
+
+styles = ['bd', 'r^']
+
+for i, (f, label) in enumerate(files.items()):
+    with open(f) as stream:
+        timings = yaml.load(stream)
+
+    timings.sort(key=lambda x:int(x[1]))
+
+    timings = [(name, size, sum(t)/len(t)) for name, size, t in timings]
+    sizes = [t[1] for t in timings]
+    times = [t[2] for t in timings]
+
+    plt.plot(sizes, times, styles[i], label=label)
+
+plt.ylabel('Time (seconds)', fontsize=18)
+plt.xlabel('Package DAG size (nodes)', fontsize=18)
+
+plt.legend(loc=2)
+
+plt.savefig('concretization-times.pdf')
